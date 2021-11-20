762b1f4f3e4dfc10138c894905ddee6ba2985529
hong <lun3322@126.com>
2021-11-20 21:23:06 +0800

    增加 默认接口

---
+++ src/hong-web/src/main/java/com/huaface/hong/web/IndexController.java
@@ -0,0 +1,17 @@
+package com.huaface.hong.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chen
+ */
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String index() {
+        return "running";
+    }
+}
